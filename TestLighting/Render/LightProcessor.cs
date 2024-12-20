e6f239655ab62665a59a96159ffa824d6e509f45
Danil <145000945+Arharim@users.noreply.github.com>
2024-12-20 23:42:35 +0500

    Update LightProcessor.cs

--- TestLighting/Render/LightProcessor.cs
+++ TestLighting/Render/LightProcessor.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using TestLighting.Lighting;
+using LightningSample.Lighting;
 
-namespace TestLighting.Render
+namespace LightningSample.Render
 {
     public class LightProcessor
     {
@@ -14,7 +14,25 @@ namespace TestLighting.Render
             _maxLightsPerPass = maxLightsPerPass;
         }
 
-        public List<List<Light>> SplitLightsIntoGroups(List<Light> lights)
+        public List<List<Light>> SplitPointLightsIntoGroups(List<Light> pointLights)
+        {
+            return SplitLightsIntoGroups(pointLights);
+        }
+
+        public List<List<SpotLight>> SplitSpotLightsIntoGroups(List<SpotLight> spotLights)
+        {
+            return SplitLightsIntoGroups(spotLights);
+        }
+
+        public List<List<T>> SplitLightsIntoGroups<T>(List<T> lights)
+        {
+            return Enumerable
+                .Range(0, (int)Math.Ceiling((double)lights.Count / _maxLightsPerPass))
+                .Select(i => lights.Skip(i * _maxLightsPerPass).Take(_maxLightsPerPass).ToList())
+                .ToList();
+        }
+
+        public List<List<Light>> SplitLightsIntoGroups1(List<Light> lights)
         {
             return Enumerable
                 .Range(0, (int)Math.Ceiling((double)lights.Count / _maxLightsPerPass))
